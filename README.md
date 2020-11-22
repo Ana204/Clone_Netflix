cd7ad69e135af9aad4d4e2b052ef41c1215d12e0
Ana204 <60896884+Ana204@users.noreply.github.com>
2020-11-22 16:01:53 -0300

    img README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Clone_Netflix
+
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default ({black}) => {
         <header className={black ? 'black' : ''}>
             <div className="header--logo">
                 <a href="/" >
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="netflix" />
+                    <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="netflix" />
                 </a>
             </div>
 
